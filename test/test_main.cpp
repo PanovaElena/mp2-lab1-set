b4e41d0be57608796d57b570416142d6d39dfd34
klusecd <klusecd@gmail.com>
2020-09-16 17:27:45 +0300

    new_cimmit

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,3 +4,9 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+TEST(testCase, can_add_2_number) {
+	int a = 3, b = 4;
+	int c = a + b;
+	ASSERT_EQ(7, c);
+}
