89a4814bf0c5f9599e64d1e6ae15b85e784b4bda
Орлов Максим Денисович <orlov.m@ccam.cmc.unn.net>
2020-09-16 17:28:00 +0300

    Aubameyang will score 40+ goals this season

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,13 @@
 #include <gtest.h>
 
+TEST(TestCase, can_add_2_numbers) {
+	int a = 3, b = 4;
+
+	int c = a + b;
+
+	ASSERT_EQ(c, 6);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
