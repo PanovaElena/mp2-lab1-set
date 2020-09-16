49122f1f40122843244dc176ef47b826e46c6689
Ненашева Яна Алексеевна <nenasheva.ja@ccam.cmc.unn.net>
2020-09-16 17:29:16 +0300

    Яна

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,3 +4,10 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+TEST(TestCase, can_add_2_numbers)
+{
+	int a = 3, b = 4;
+	int c = a + b;
+		ASSERT_EQ(7, c);
+}
